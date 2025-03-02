7da2ed4b0cc11782c3e65f2499a2d1d403c94f87
Oleg <olegmaslakov47@gmail.com>
2025-03-02 22:57:49 +0200

    Add new updates

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
-"use strict";
+'use strict';
 
-let currentNumber = 2;
-const lastNumber = 100;
-
-// Внешний цикл перебираем все чсла от 1 до 100 но начинаем с 2 так 1 - число исключение
-outerLoop: while (currentNumber <= lastNumber) {
+for (let currentNumber = 2; currentNumber <= 100; currentNumber++) {
   let isSimple = true;
-  let simpleNumber = 2;
-
-  // Внутренний цикл проверяет делители от 2 до sqrt(currentNumber)
-  innerLoop: while (simpleNumber <= Math.sqrt(currentNumber)) {
+  innerloop: for (
+    let simpleNumber = 2;
+    simpleNumber <= Math.sqrt(currentNumber);
+    simpleNumber++
+  ) {
     if (currentNumber % simpleNumber === 0) {
       isSimple = false;
-      break innerLoop; // Прерываем внутренний цикл
+      break innerloop;
     }
-    simpleNumber++;
   }
-
   if (isSimple) {
     console.log(currentNumber);
   }
-
-  currentNumber++;
 }
